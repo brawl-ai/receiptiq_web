7fb9bca2d193ad283d4675e0e04f03b0f7494c99
peterkahenya <kahenya0@gmail.com>
2025-09-01 12:21:38 +0300

    refresh homepage and remove mantine

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,3 +1,5 @@
+import { createTheme, MantineProvider } from "@mantine/core"
+import { Notifications } from "@mantine/notifications"
 import { Metadata } from "next"
 import { Suspense } from "react"
 
@@ -6,10 +8,19 @@ export const metadata: Metadata = {
     title: "Login"
 }
 
+const theme = createTheme({
+    fontFamily: "Open Sans, sans-serif",
+});
+
 export default function LoginLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    return <Suspense>{children}</Suspense>
+    return <Suspense>
+        <MantineProvider theme={theme} defaultColorScheme="auto">
+            <Notifications />
+            {children}
+        </MantineProvider>
+    </Suspense>
 }
