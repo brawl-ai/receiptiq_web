c4ea2e5255febda7a1906c51653aedcc2c815e41
peterkahenya <kahenya0@gmail.com>
2025-09-01 14:29:11 +0300

    update login  and password reset flows

---
+++ app/login/login_form.tsx
@@ -0,0 +1,107 @@
+"use client"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { Checkbox } from "@/components/ui/checkbox"
+
+export function LoginForm({errors, loading, handleSubmit}: {errors?:string[], loading?:boolean, handleSubmit: (values: any) => void } & React.HTMLAttributes<HTMLDivElement>) {
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+        remember_me: false
+    })
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault(); 
+        handleSubmit(formState);
+    }
+
+    return (
+    <div className={cn("flex flex-col gap-6")}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={onSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-muted-foreground text-balance">
+                  Login to your ReceiptIQ account
+                </p>
+              </div>
+                {errors && <div className="text-center text-red-500 font-medium">
+                    {errors.map((e, id) => <div className={"text-red-500"} key={id}>{e}</div>)}
+                </div>}
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                    id="email"
+                    type="email"
+                    placeholder="you@example.com"
+                    required
+                    autoComplete="email"
+                    value={formState.email}
+                    onChange={(e) => setFormState({ ...formState, email: e.target.value })}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    data-umami-event="forgot_password_link@login"
+                    href="/password/forgot"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                    id="password"
+                    type="password"
+                    required
+                    value={formState.password}
+                    onChange={(e) => setFormState({ ...formState, password: e.target.value })}
+                />
+              </div>
+              <div className="flex items-center gap-3">
+                <Checkbox id="terms" checked={formState.remember_me} onCheckedChange={(e) => setFormState({ ...formState, remember_me: true })} />
+                <Label htmlFor="terms">Remember me</Label>
+            </div>
+              
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={loading}
+                data-umami-event="login_button@login_form"
+              >
+                {loading ? "Logging in..." : "Login"}
+              </Button>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a 
+                    data-umami-event="create_account_link@login"
+                    href="/signup"
+                    className="underline underline-offset-4"
+                >
+                  Create account
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="relative grid md:block">
+            <img
+              src="/assets/images/bg2.jpg"
+               alt="Authentication background"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.9] dark:contrast-more"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="/docs/ReceiptIQ Terms and Conditions.pdf">Terms of Service</a>
+      </div>
+    </div>
+  )
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import {
-    TextInput,
-    Paper,
-    Title,
-    Container,
-    Button,
-    Text,
-    Anchor,
-    Stack,
-    Box,
-    Image,
-    Group,
-    PasswordInput,
-    Flex,
-    Checkbox,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { notifications } from "@mantine/notifications";
+import { useTheme } from "next-themes";
+import { IconSun, IconMoon } from '@tabler/icons-react';
+import { useSearchParams } from "next/navigation";
 import { useAuthContext } from "../stores/auth_store";
+import { LoginForm } from "./login_form";
+import { LoginRequest } from "../types";
+import { DotPattern } from "@/components/magicui/dot-pattern";
 
 export default function LoginPage() {
     const searchParams = useSearchParams()
@@ -29,20 +16,9 @@ export default function LoginPage() {
     const [errors, setErrors] = useState([]);
     const login = useAuthContext((s)=>s.login);
     const redirect = searchParams.get("redirect");
+    const { theme, setTheme } = useTheme();
 
-    const form = useForm({
-        initialValues: {
-            email: "",
-            password: "",
-            remember_me: false
-        },
-        validate: {
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : "invalid email"),
-            password: (value) => value.length < 1 ? "password is required" : null
-        },
-    });
-
-    const handleSubmit = async (values: typeof form.values) => {
+    const handleSubmit = async (values: LoginRequest) => {
         setLoading(true);
         try {
             console.log(values)
@@ -60,97 +36,39 @@ export default function LoginPage() {
                 errors.push(error.response?.data?.detail)
             }
             setErrors(errors)
-            notifications.show({
-                position: "top-right",
-                title: "Signup Error",
-                message: String(errors) || "Failed to sign up",
-                color: "red",
-            });
         } finally {
             setLoading(false);
         }
     };
 
     return (
-        <Group h="100vh" w="100%" gap={0}>
-            <Box w="50%" h="100%" visibleFrom="md">
-                <Image
-                    src="/assets/images/bg2.jpg"
-                    alt="Authentication background"
-                    h="100%"
-                    style={{ objectFit: "cover" }}
-                />
-            </Box>
-            <Box
-                w={{ base: "100%", md: "50%" }}
-                h="100%"
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                <Container size={420} w="100%">
-                    <Title ta="center" fw={900}>
-                        Login
-                    </Title>
-                    <Text c="dimmed" size="sm" ta="center" mt={5}>
-                        {"Don't have an account yet? "}
-                        <Anchor
-                            data-umami-event="create_account_link@login"
-                            component={Link}
-                            href="/signup"
-                            size="sm"
-                        >
-                            Create account
-                        </Anchor>
-                    </Text>
-
-                    <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                        {errors.map((e, id) => <Text c={"red"} key={id}>{e}</Text>)}
-                        <form onSubmit={form.onSubmit(handleSubmit)}>
-                            <Stack>
-                                <TextInput
-                                    label="Email"
-                                    placeholder="you@example.com"
-                                    required
-                                    size="md"
-                                    {...form.getInputProps("email")}
-                                />
-                                <PasswordInput
-                                    label="Password"
-                                    placeholder="********"
-                                    required
-                                    size="md"
-                                    {...form.getInputProps("password")}
-                                />
-                            </Stack>
-                            <Flex justify={"space-between"} mt={"md"}>
-                                <Checkbox label="Remember me" {...form.getInputProps("remember_me")} />
-                                <Anchor
-                                    data-umami-event="forgot_password_link@login"
-                                    component="a"
-                                    size="sm"
-                                    href="/password/forgot"
-                                >
-                                    Forgot password?
-                                </Anchor>
-                            </Flex>
-
-                            <Button
-                                data-umami-event="login_button@login"
-                                fullWidth
-                                mt="xl"
-                                size="md"
-                                type="submit"
-                                loading={loading}
-                            >
-                                Login
-                            </Button>
-                        </form>
-                    </Paper>
-                </Container>
-            </Box>
-        </Group>
+        <div className="relative bg-muted min-h-svh flex flex-col">
+            <DotPattern className="opacity-40" />
+            {/* Header */}
+            <header className="w-full flex items-center justify-around px-6 py-4 bg-transparent">
+                <div className="flex items-center gap-3">
+                    <Link href="/" className="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="160" height="44" viewBox="0 0 220 64" className="text-blue-500 dark:text-blue-500">
+                            <rect x="2" y="2" width="40" height="40" rx="5" stroke="currentColor" strokeWidth="2" fill="transparent" />
+                            <path d="M12 22 L16 18 L20 26 L24 18 L28 26 L32 18 L36 26" stroke="currentColor" strokeWidth="3" fill="none" />
+                            <text x="50" y="32" fontFamily="Arial, sans-serif" fill="currentColor" fontSize="20" fontWeight="bold">ReceiptIQ</text>
+                        </svg>
+                    </Link>
+                </div>
+                <div className="flex items-center gap-2">
+                    {theme === 'dark' ?
+                        <IconSun size={22} className="cursor-pointer text-gray-300" onClick={() => setTheme('light')} />
+                        :
+                        <IconMoon size={22} className="cursor-pointer" onClick={() => setTheme('dark')} />
+                    }
+                </div>
+            </header>
+            {/* Main login form */}
+            <div className=" relative flex flex-1 items-center justify-center p-6 md:p-10">
+                <div className="w-full max-w-sm md:max-w-3xl">
+                    <LoginForm errors={errors} loading={loading} handleSubmit={handleSubmit}/>
+                </div>
+            </div>
+        </div>
     );
 }
