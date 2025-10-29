f8b2ce0d190c7046aa80c813f8207f88808ce890
peterkahenya <kahenya0@gmail.com>
2025-10-29 08:10:19 +0300

    improve homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,8 @@ import {
 } from '@tabler/icons-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { GridPattern } from '@/components/ui/grid-pattern';
 import { Separator } from '@/components/ui/separator';
 import Footer01 from '@/components/ui/footer';
-import { RainbowButton } from '@/components/ui/rainbow-button';
 import {
   Card,
   CardContent,
@@ -29,44 +27,38 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { useTheme } from 'next-themes';
+import Image from "next/image";
+import { BorderBeam } from '@/components/ui/border-beam';
 
 export default function ReceiptIQHomepage() {
   const [opened, setOpened] = useState(false);
   const { theme, setTheme } = useTheme()
 
   return (
-    <div className="relative min-h-screen w-full overflow-hidden">
-      <GridPattern
-        className="absolute min-h-screen inset-0 -z-20 opacity-20 dark:bg-stone-900 dark:opacity-100"
-        width={20}
-        height={20}
-        strokeDasharray={"4 2"}
-        x={-5} 
-        y={-5}
-      />
+    <div className="relative min-h-screen w-full">
       {/* Header */}
-      <header className="sticky top-0 z-20 backdrop-transparent">
-        <div className="mx-auto flex h-[70px] max-w-7xl items-center justify-between px-4">
+      <header className="sticky top-0 backdrop-blur-md bg-transparent/50 z-50">
+        <div className="mx-auto flex h-[60px] max-w-7xl items-center justify-between px-4">
           <div className="flex items-center gap-3">
             {/* Mobile burger */}
-            <IconLayoutSidebarFilled 
-                onClick={() => setOpened(!opened)}
-                size={24}
-                className="sm:hidden dark:text-white text-blue-500"
+            <IconLayoutSidebarFilled
+              onClick={() => setOpened(!opened)}
+              size={24}
+              className="sm:hidden dark:text-white text-blue-500"
             />
             <div className="flex items-center gap-3">
               {/* Logo */}
               <Link href="/" className="flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="220" height="64" viewBox="0 0 220 64" className="mt-2 text-blue-500 dark:text-blue-500">
+                <svg xmlns="http://www.w3.org/2000/svg" width="200" height="45" viewBox="0 0 220 64" className="mt-2">
                   {/* Rounded square on the left */}
-                  <rect x="2" y="2" width="60" height="60" rx="5" stroke="currentColor" strokeWidth="2" fill="transparent" />
+                  <rect x="2" y="2" width="60" height="60" rx="5" stroke="currentColor" strokeWidth="2" fill="white" />
                   {/* Squiggly line in the center */}
-                  <path d="M12 32 L16 28 L20 36 L24 28 L28 36 L32 28 L36 36 L40 28 L44 36 L48 28 L52 36 L55 32" stroke="currentColor" strokeWidth="3" fill="none" />
-                {/* Text next to it */}
-                <text x="75" y="40" fontFamily="Arial, sans-serif" fill="currentColor" fontSize="28" fontWeight={"bold"}>
-                  ReceiptIQ
-                </text>
-              </svg>
+                  <path d="M12 32 L16 28 L20 36 L24 28 L28 36 L32 28 L36 36 L40 28 L44 36 L48 28 L52 36 L55 32" stroke="black" strokeWidth="3" fill="none" />
+                  {/* Text next to it */}
+                  <text x="75" y="40" fontFamily="Roboto, sans-serif" fill="currentColor" fontSize="30">
+                    ReceiptIQ
+                  </text>
+                </svg>
               </Link>
             </div>
           </div>
@@ -82,8 +74,8 @@ export default function ReceiptIQHomepage() {
               <Link href="/signup">Get Started</Link>
             </Button>
             {theme === 'dark' ?
-            <IconSun size={16} className="mr-2 text-gray-300" onClick={() => setTheme('light')} />
-            :<IconMoon size={16} className="mr-2" onClick={() => setTheme('dark')} />
+              <IconSun size={16} className="mr-2 text-gray-300" onClick={() => setTheme('light')} />
+              : <IconMoon size={16} className="mr-2" onClick={() => setTheme('dark')} />
             }
           </nav>
         </div>
@@ -118,27 +110,36 @@ export default function ReceiptIQHomepage() {
         {/* Hero Section */}
         <section className="py-10">
           <div className="mx-auto max-w-7xl px-4 text-center">
-            <h1 className="mx-auto max-w-3xl text-4xl font-bold leading-tight sm:text-4xl md:text-4xl dark:text-white">
-              One tool to{" "}
-              <span className="text-blue-500">extract data</span> from receipts
-              and manage your workflow
+            <h1 className="mx-auto max-w-3xl text-6xl font-bold leading-tight sm:text-4xl md:text-6xl dark:text-white">
+              AI-Powered <br />
+              Data Extraction
             </h1>
             <p className="mx-auto mt-6 max-w-xl text-lg text-muted-foreground leading-relaxed">
-              ReceiptIQ helps teams process receipts faster, smarter and more
-              efficiently, delivering the visibility and data-driven insights to
-              mitigate risk and ensure compliance.
+              ReceiptIQ helps teams process receipts faster, smarter and more efficiently, delivering the flexibilty and accuracy.
             </p>
             <div className="mt-8 flex justify-center">
-              <RainbowButton data-umami-event="get_started@home_herosection" asChild size='lg'> 
+              <Button data-umami-event="get_started@home_herosection" asChild size='lg'>
                 <Link href="/signup">
                   <IconArrowRight size={18} /> Get Started
                 </Link>
-              </RainbowButton>
+              </Button>
+            </div>
+            <div className='relative w-full bg-transparent px-2 pt-10 pb-20 md:py-16'>
+              <div className="gradient -translate-x-1/2 absolute inset-0 left-1/2 h-1/4 w-3/4 animate-image-glow blur-[5rem] md:top-[10%] md:h-1/3"></div>
+              <div className="-m-2 lg:-m-4 rounded-xl bg-opacity-50 p-2 ring-1 ring-foreground/20 ring-inset backdrop-blur-3xl lg:rounded-2xl">
+                <Image src={"/assets/images/app_page1.avif"} alt='app page 1' />
+                <BorderBeam
+                  duration={4}
+                  size={300}
+                  reverse
+                  className="from-transparent via-green-500 to-transparent"
+                />
+              </div>
             </div>
           </div>
         </section>
 
-        <Separator className='px-10'/>
+        <Separator className='px-10' />
         {/* Features Section */}
         <section className="py-10">
           <div className="mx-auto max-w-7xl px-4 text-center">
@@ -189,15 +190,15 @@ export default function ReceiptIQHomepage() {
                   color: "bg-blue-100 text-blue-500",
                 },
               ].map((f, i) => (
-                  <Card key={i}>
-                    <CardHeader className={`flex items-center gap-4`}>
-                      <CardTitle className={`${f.color} flex h-12 w-12 items-center justify-center rounded-md`}>{f.icon}</CardTitle>
-                      <CardDescription className="text-xl font-semibold dark:text-white">{f.title}</CardDescription>
-                    </CardHeader>
-                    <CardContent className="text-muted-foreground leading-relaxed">
-                      <p>{f.desc}</p>
-                    </CardContent>
-                  </Card>
+                <Card key={i}>
+                  <CardHeader className={`flex items-center gap-4`}>
+                    <CardTitle className={`${f.color} flex h-12 w-12 items-center justify-center rounded-md`}>{f.icon}</CardTitle>
+                    <CardDescription className="text-xl font-semibold dark:text-white">{f.title}</CardDescription>
+                  </CardHeader>
+                  <CardContent className="text-muted-foreground leading-relaxed">
+                    <p>{f.desc}</p>
+                  </CardContent>
+                </Card>
               ))}
             </div>
           </div>
